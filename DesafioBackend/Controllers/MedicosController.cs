20b7198e461e61f5db602e3698bea9af86f3b13f
Jorge Jr <jorgenjr@outlook.com>
2020-08-18 15:15:44 -0300

    Adição do contexto do banco de dados Adição de controller para as rotas de Médico

---
+++ DesafioBackend/Controllers/MedicosController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DesafioBackend.Models;
+
+namespace DesafioBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicosController : ControllerBase
+    {
+        private readonly MedicoContext _context;
+
+        public MedicosController(MedicoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Medicos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Medico>>> GetMedicos()
+        {
+            return await _context.Medicos.ToListAsync();
+        }
+
+        // GET: api/Medicos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Medico>> GetMedico(Guid id)
+        {
+            var medico = await _context.Medicos.FindAsync(id);
+
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            return medico;
+        }
+
+        // PUT: api/Medicos/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedico(Guid id, Medico medico)
+        {
+            if (id != medico.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(medico).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MedicoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Medicos
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Medico>> PostMedico(Medico medico)
+        {
+            _context.Medicos.Add(medico);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMedico", new { id = medico.id }, medico);
+        }
+
+        // DELETE: api/Medicos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Medico>> DeleteMedico(Guid id)
+        {
+            var medico = await _context.Medicos.FindAsync(id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            _context.Medicos.Remove(medico);
+            await _context.SaveChangesAsync();
+
+            return medico;
+        }
+
+        private bool MedicoExists(Guid id)
+        {
+            return _context.Medicos.Any(e => e.id == id);
+        }
+    }
+}
--- DesafioBackend/Startup.cs
+++ DesafioBackend/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DesafioBackend.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DesafioBackend
 {
@@ -25,6 +20,11 @@ namespace DesafioBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MedicoContext>(opt =>
+                opt.UseNpgsql("Medicos"));
+            services.AddDbContext<MedicoContext>(opt =>
+                opt.UseNpgsql("Especialidades"));
+
             services.AddControllers();
         }
 
